8e356b0f4a6f532e24c6b8e409af89b4d69ae611
Nina Mutty <ninamutty@gmail.com>
2016-09-28 16:41:12 -0700

    completed wave 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,22 +23,34 @@ class TasksController < ApplicationController
   end
 
   def edit
+    show
   end
 
   def update
+    show
+
+    @onetask[:title] = params["title"]
+    @onetask[:description] = params["description"]
+    @onetask[:status] = params["status"]
+    @onetask[:status] == "yes" ? @onetask[:completed] = Date.new : @onetask[:completed] = nil
   end
 
   def destroy
   end
 
   def create
+    @params = params
+    @title = params[:title]
+    @description = params[:description]
+    @status = "no"
+    @completed = nil
   end
 
   def self.alltasks
     [
-      {id: 1, title: "Groceries", description: "Go buy eggs, milk, cheese, yogurt, icecream, half and half, and any other dairy product you can think of", status: false, completed: nil},
-      {id: 2, title: "Workout", description: "Get your heart a pumpin!", status: true, completed: Date.new},
-      {id: 3, title: "Homework", description: "Time to get smart!", status: false, completed: nil}
+      {id: 1, title: "Groceries", description: "Go buy eggs, milk, cheese, yogurt, icecream, half and half, and any other dairy product you can think of", status: "no", completed: nil},
+      {id: 2, title: "Workout", description: "Get your heart a pumpin!", status: "yes", completed: Date.new},
+      {id: 3, title: "Homework", description: "Time to get smart!", status: "no", completed: nil}
     ]
   end
 
