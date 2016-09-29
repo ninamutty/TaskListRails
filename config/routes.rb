b78d2323b1e7ae25b47743fe3bdd9cf7029b5c7c
Nina Mutty <ninamutty@gmail.com>
2016-09-29 15:30:54 -0700

    updated Gemfile with awesome print

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'tasks/new', as: 'new'
   post 'tasks' => 'tasks#create', as: 'create'
 
-  post 'tasks/create' => 'tasks#create', as: 'create'
+  post 'tasks/create' => 'tasks#create', as: 'tasks_create'
 
 
   get 'tasks/:id/edit' => 'tasks#edit', as: 'edit'
