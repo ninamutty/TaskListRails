7740a53e2e1f0c22fdb282073aa59054e1b6a67c
Nina Mutty <ninamutty@gmail.com>
2016-10-18 10:01:47 -0700

    created user model to use oauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929232849) do
+ActiveRecord::Schema.define(version: 20161018170109) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20160929232849) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.boolean  "complete"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "uid"
+    t.string   "name"
+    t.string   "provider"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
